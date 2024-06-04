df7198725f7fc00440ed6b42913bcf3e690f1d87
Mitho Mizuno <mithomizuno@gmail.com>
2024-06-05 04:41:06 +0700

    Minor change

--- raknet/raknet.py
+++ raknet/raknet.py
@@ -1,9 +1,11 @@
+from typing import Final
+
 __all__ = [
     "RakNet"
 ]
 
 
-class RakNet:
+class RakNet(Final):
     # Default vanilla RakNet protocol version that this library implements. Things using RakNet can override this
     # protocol version with something different.
     DEFAULT_PROTOCOL_VERSION = 6
